ba574557360694560a4c66db963b9d9fa4822de3
Wiibuntu <146908244+Wiibuntu@users.noreply.github.com>
2025-01-06 21:23:30 -0500

    menu

--- benchmark.cpp
+++ benchmark.cpp
@@ -29,24 +29,24 @@ int main(int argc, char *argv[]) {
     gtk_label_set_use_markup(GTK_LABEL(label), TRUE);
     gtk_box_pack_start(GTK_BOX(vbox), label, TRUE, TRUE, 0);
 
-    button1 = gtk_button_new_with_label("Run Executable 1");
-    g_signal_connect(button1, "clicked", G_CALLBACK(on_button_clicked), "./executable1");
+    button1 = gtk_button_new_with_label("Sphere");
+    g_signal_connect(button1, "clicked", G_CALLBACK(on_button_clicked), "./opt/WiiBench-Apps/Sphere");
     gtk_box_pack_start(GTK_BOX(vbox), button1, TRUE, TRUE, 0);
 
-    button2 = gtk_button_new_with_label("Run Executable 2");
-    g_signal_connect(button2, "clicked", G_CALLBACK(on_button_clicked), "./executable2");
+    button2 = gtk_button_new_with_label("Cube");
+    g_signal_connect(button2, "clicked", G_CALLBACK(on_button_clicked), "./opt/WiiBench-Apps/Cube");
     gtk_box_pack_start(GTK_BOX(vbox), button2, TRUE, TRUE, 0);
 
-    button3 = gtk_button_new_with_label("Run Executable 3");
-    g_signal_connect(button3, "clicked", G_CALLBACK(on_button_clicked), "./executable3");
+    button3 = gtk_button_new_with_label("Polygon");
+    g_signal_connect(button3, "clicked", G_CALLBACK(on_button_clicked), "./opt/WiiBench-Apps/Polygon");
     gtk_box_pack_start(GTK_BOX(vbox), button3, TRUE, TRUE, 0);
 
-    button4 = gtk_button_new_with_label("Run Executable 4");
-    g_signal_connect(button4, "clicked", G_CALLBACK(on_button_clicked), "./executable4");
+    button4 = gtk_button_new_with_label("Tree");
+    g_signal_connect(button4, "clicked", G_CALLBACK(on_button_clicked), "./opt/WiiBench-Apps/Tree");
     gtk_box_pack_start(GTK_BOX(vbox), button4, TRUE, TRUE, 0);
 
-    button5 = gtk_button_new_with_label("Run Executable 5");
-    g_signal_connect(button5, "clicked", G_CALLBACK(on_button_clicked), "./executable5");
+    button5 = gtk_button_new_with_label("CraftWorld");
+    g_signal_connect(button5, "clicked", G_CALLBACK(on_button_clicked), "./opt/WiiBench-Apps/CraftWorld");
     gtk_box_pack_start(GTK_BOX(vbox), button5, TRUE, TRUE, 0);
 
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
