040ec7d4e330b8430f022b3454172931be501cc0
Wiibuntu <146908244+Wiibuntu@users.noreply.github.com>
2025-01-06 20:55:08 -0500

    Update benchmark.cpp

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,136 +1,60 @@
-#include <GL/glut.h>
-#include <cmath>
-#include <cstdlib>
-#include <vector>
+#include <gtk/gtk.h>
 
-float angle = 0.0f;
-float lightAngle = 0.0f;
-GLuint texture;
-const int worldSize = 50;
-float terrain[worldSize][worldSize];
-
-const int checkImageWidth = 64;
-const int checkImageHeight = 64;
-GLubyte checkImage[checkImageHeight][checkImageWidth][4];
-
-float perlinNoise(int x, int y) {
-    float scale = 0.1f;
-    return (float)(sin(x * scale) * cos(y * scale) * 10);
+void on_button_clicked(GtkWidget *widget, gpointer data) {
+    system((char *)data);
 }
 
-void generateTerrain() {
-    for (int x = 0; x < worldSize; x++) {
-        for (int y = 0; y < worldSize; y++) {
-            terrain[x][y] = perlinNoise(x, y);
-        }
-    }
-}
+int main(int argc, char *argv[]) {
+    GtkWidget *window;
+    GtkWidget *vbox;
+    GtkWidget *label;
+    GtkWidget *button1;
+    GtkWidget *button2;
+    GtkWidget *button3;
+    GtkWidget *button4;
+    GtkWidget *button5;
 
-void makeCheckImage() {
-    for (int i = 0; i < checkImageHeight; i++) {
-        for (int j = 0; j < checkImageWidth; j++) {
-            int c = ((((i & 8) == 0) ^ ((j & 8) == 0))) * 255;
-            checkImage[i][j][0] = (GLubyte)c;
-            checkImage[i][j][1] = (GLubyte)c;
-            checkImage[i][j][2] = (GLubyte)c;
-            checkImage[i][j][3] = (GLubyte)255;
-        }
-    }
-}
+    gtk_init(&argc, &argv);
 
-void initTexture() {
-    makeCheckImage();
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, checkImageWidth, checkImageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, checkImage);
-    glEnable(GL_TEXTURE_2D);
-}
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Wiibench");
+    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 500);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
 
-void initRayTracedLighting() {
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-    glEnable(GL_COLOR_MATERIAL);
-    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
-    glEnable(GL_NORMALIZE);
-}
+    vbox = gtk_vbox_new(FALSE, 10);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
 
-void updateLightPosition() {
-    GLfloat light_position[] = { 10.0f * sin(lightAngle), 10.0f, 10.0f * cos(lightAngle), 1.0f };
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-}
+    label = gtk_label_new("<span font='40'>Wiibench</span>");
+    gtk_label_set_use_markup(GTK_LABEL(label), TRUE);
+    gtk_box_pack_start(GTK_BOX(vbox), label, TRUE, TRUE, 0);
 
-void drawBlock(float x, float y, float z) {
-    glPushMatrix();
-    glTranslatef(x, y, z);
-    glutSolidCube(1.0f);
-    glPopMatrix();
-}
+    button1 = gtk_button_new_with_label("Run Executable 1");
+    g_signal_connect(button1, "clicked", G_CALLBACK(on_button_clicked), "./executable1");
+    gtk_box_pack_start(GTK_BOX(vbox), button1, TRUE, TRUE, 0);
 
-void drawTerrain() {
-    for (int x = 0; x < worldSize; x++) {
-        for (int y = 0; y < worldSize; y++) {
-            float height = terrain[x][y];
-            drawBlock(x - worldSize / 2, height, y - worldSize / 2);
-        }
-    }
-}
+    button2 = gtk_button_new_with_label("Run Executable 2");
+    g_signal_connect(button2, "clicked", G_CALLBACK(on_button_clicked), "./executable2");
+    gtk_box_pack_start(GTK_BOX(vbox), button2, TRUE, TRUE, 0);
 
-void display() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();
+    button3 = gtk_button_new_with_label("Run Executable 3");
+    g_signal_connect(button3, "clicked", G_CALLBACK(on_button_clicked), "./executable3");
+    gtk_box_pack_start(GTK_BOX(vbox), button3, TRUE, TRUE, 0);
 
-    glTranslatef(0.0f, -10.0f, -50.0f);
-    glRotatef(angle, 0.0f, 1.0f, 0.0f);
+    button4 = gtk_button_new_with_label("Run Executable 4");
+    g_signal_connect(button4, "clicked", G_CALLBACK(on_button_clicked), "./executable4");
+    gtk_box_pack_start(GTK_BOX(vbox), button4, TRUE, TRUE, 0);
 
-    updateLightPosition();
-    
-    glBindTexture(GL_TEXTURE_2D, texture);
-    drawTerrain();
+    button5 = gtk_button_new_with_label("Run Executable 5");
+    g_signal_connect(button5, "clicked", G_CALLBACK(on_button_clicked), "./executable5");
+    gtk_box_pack_start(GTK_BOX(vbox), button5, TRUE, TRUE, 0);
 
-    glutSwapBuffers();
-}
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-void update(int value) {
-    angle += 1.0f;
-    if (angle > 360) {
-        angle -= 360;
-    }
+    gtk_widget_show_all(window);
 
-    lightAngle += 0.01f;
-    if (lightAngle > 2 * M_PI) {
-        lightAngle -= 2 * M_PI;
-    }
+    gtk_main();
 
-    glutPostRedisplay();
-    glutTimerFunc(16, update, 0);
-}
-
-void reshape(int w, int h) {
-    glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(45.0, (float)w / (float)h, 1.0, 200.0);
-    glMatrixMode(GL_MODELVIEW);
-}
-
-int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(800, 600);
-    glutCreateWindow("Minecraft-Style Terrain with Rotating Light and Perlin Noise");
-
-    glEnable(GL_DEPTH_TEST);
-    initRayTracedLighting();
-    initTexture();
-    generateTerrain();
-
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutTimerFunc(25, update, 0);
-
-    glutMainLoop();
     return 0;
 }
 
